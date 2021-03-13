3e0d7ca381aed4683aeb3f0d5de0da1a78dcb8f8
Suada Rantisi <suadarantisi@gmail.com>
2021-03-13 22:55:58 +0700

    Perbaikin app.js

--- app.js
+++ app.js
@@ -84,6 +84,14 @@ app.post("/update", (req, res) => {
 	);
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+// app.listen(port, () => {
+// 	console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+	console.log(
+		"Express server listening on port %d in %s mode",
+		this.address().port,
+		app.settings.env
+	);
 });
